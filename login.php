17b42aeca42d4f4427d1802a2d8d40f9adee33e4
DESKTOP-5PI6AT4\Daniel Ingram <ingramdaniel5@gmail.com>
2018-11-01 01:47:24 -0400

    Completed Login Screen revision 1.0. Also implemented its ability to be able to go to the main page when the login is correct.  The username and password is admin admin.

--- login.php
+++ login.php
@@ -1,4 +1,19 @@
-<?php
-  // Here is where the login page will be produced
-  echo"<h1>Hello! The login page goes here!</h1>";
-?>
+<html>
+  <head>
+    <title> Photo Storage Login </title>
+    	<link rel="stylesheet" type="text/css" href="style.css">
+    <body>
+    	<div class="loginbox">
+    	<h1> Login Here </h1>
+      	<form method="POST" action="main.php">
+      		<p> Username </p>
+      		  <input type="text" name="username" placeholder="Enter Username:">
+      		<p> Password </p>
+      		  <input type="password" name="password" placeholder = "Enter Password:">
+          </br>
+      		  <input type="submit" name="" value="Login">
+      	</form>
+    	</div>
+    </body>
+  </head>
+</html>
--- main.php
+++ main.php
@@ -1,23 +1,23 @@
 <?php
   // Here is where the photos will be loaded into the page and the page will be laid out
-  
+  //Username and password for testing purposes:
+  $username = 'admin';
+  $password = 'admin';
   //Commented out until login page ready
-  //if ($_GET['username'] == $username && $_GET['password'] == $password){}
-  if (true)
+  if ($_POST['username'] == $username && $_POST['password'] == $password)
   {
-	//code that displays all files in STORAGE directory
-    $files = glob("STORAGE/*.*");
-	for ($i=0; $i<count($files); $i++)
-	{
-		$num = $files[$i];
-		echo '<img style=\"width:250px;height:250px;" src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
-		echo "</br>";
-	}
-
+  	//code that displays all files in STORAGE directory
+      $files = glob("STORAGE/*.*");
+  	for ($i=0; $i<count($files); $i++)
+  	{
+  		$num = $files[$i];
+  		echo '<img style=\"width:250px;height:250px;" src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
+  		echo "</br>";
+  	}
   }
   else
   {
     //Puts you back at the login page.
-
+    include('login.php');
   }
 ?>
