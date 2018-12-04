d72b3e591158a43bd5cd8a061d37b42d437c45be
DESKTOP-5PI6AT4\Daniel Ingram <ingramdaniel5@gmail.com>
2018-12-04 15:23:39 -0500

    fixed background image not showing on failed login

--- main.php
+++ main.php
@@ -4,30 +4,43 @@
 @brief main program of the project. User enters their username and Password, check validity, calls upload.php to load images
 @author Daniel Ingram, Mohamed Jallow
 **/
-
 ?>
 
 
-<?php //Links the style sheet to the page ?>
 
-<body class="MainBody">
+<?php
+//Links the style sheet to the page
+// Here is where the photos will be loaded into the page and the page will be laid out
+//Username and password for testing purposes:
+$username = 'bobcat';
+$password = 'bobcat123';
+
+//Default fillers for user name and Password
+$submittedUserName = '';
+$submittedPassword = '';
+if (isset($_POST['username']) && isset($_POST['password']))
+{
+  $submittedUserName = $_POST['username'];
+  $submittedPassword = $_POST['password'];
+}
+//Looking for if the username and password are correct or its returning from the upload page.
+if (($submittedUserName == $username && $submittedPassword == $password) || !empty($_GET['upload']))
+{
+  echo "<body class=\"MainBody\">";
+}
+else
+{
+  echo "<body class=\"MainBody1\">";
+}
+?>
+
+
   <link rel="stylesheet" type="text/css" href="Style/style.css">
   <link rel="stylesheet" type="text/css" href="Style/bootstrap-4.1.3-dist/css/bootstrap.min.css">
   <center>
  <div class='container'>
  <div class="gallery">
   <?php
-    // Here is where the photos will be loaded into the page and the page will be laid out
-    //Username and password for testing purposes:
-    $username = 'admin';
-    $password = 'admin';
-
-    //Default fillers for user name and Password
-    $submittedUserName = '';
-    $submittedPassword = '';
-
-    //this is a test
-
 
     //This is needed because in the event the submission was successful, there would be no username
     //and password attached to the main page.
